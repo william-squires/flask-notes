72bd222adf99a5fa3541900f95dabe8c875caff9
William Squires <william.squiresii@gmail.com>
2023-03-21 14:02:43 -0500

    added User model

--- models.py
+++ models.py
@@ -14,3 +14,34 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+class User(db.Model):
+    """Models for Users"""
+
+    __tablename__ = "users"
+
+    username = db.Column(
+        db.String(20),
+        primary_key=True
+    )
+
+    password = db.Column(
+        db.String(100),
+        nullable=False,
+    )
+
+    email = db.Column(
+        db.String(50),
+        nullable=False,
+        unique=True
+    )
+
+    first_name = db.Column(
+        db.String(30),
+        nullable=False,
+    )
+
+    last_name = db.Column(
+        db.String(30),
+        nullable=False,
+    )
